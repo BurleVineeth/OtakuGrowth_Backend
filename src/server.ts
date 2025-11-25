feb25a0bcc7be2ea97e1874c9dae221e7629222a
BurleVineeth <burle.vineeth@daffodilsw.com>
2025-11-25 13:38:19 +0530

    MongoDB connection.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 import { config } from "./core/config/env";
+import { connectDB } from "./core/config/db"
 
 const PORT = config.PORT;
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
